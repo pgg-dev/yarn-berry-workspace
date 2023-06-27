3f2b2f81993c16a52483d472c0cefb1ca5969c38
pgg-dev <park.geumgyu@gmail.com>
2023-06-27 20:40:42 +0900

    Chore: create eslintrc

--- apps/my-app/src/app/page.tsx
+++ apps/my-app/src/app/page.tsx
@@ -1,6 +1,6 @@
+import { sayHello } from "@my-app/lib";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { sayHello } from "@my-app/lib";
 
 export default function Home() {
   return (
